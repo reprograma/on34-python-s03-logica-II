dc07b509df42b857049b4c3b8febe02f27e780e7
Erika Campos <erikacamposdesign@users.noreply.github.com>
2024-06-20 12:34:16 -0300

    Adiciona exerc de soma dentra da função

---
+++ exercicios/grupo_de-estudos/uso-de-funcao_soma.py
@@ -0,0 +1,12 @@
+gasto1= 56
+gasto2 = 88
+gasto3 = 20
+
+def gasto_total(g1, g2, g3):
+    return g1 + g2 + g3
+
+
+print("O total de gastos é", gasto_total(gasto1, gasto2, gasto3))
+print("mas...")
+print("Também podemos fazer já dando o valor dos argumentos:")
+print("O total de gastos é", gasto_total(55, 87, 19))
